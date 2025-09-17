6f3ab85b452ea06c2b0cd70955eccf18b4debd08
Ostap Mykhaylyak <me@ostap.dev>
2025-09-17 04:40:10 +0200

    Update users.sh

--- percona/users.sh
+++ percona/users.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
-# Gestione utenti
-manage_user() {
-    local action="$1"
-    local username="$2"
-    local password="${3:-$(generate_password)}"
-    local database="${4:-*}"
-    local host="${5:-localhost}"
-    
-    case "$action" in
-        "create")
-            log "Creazione utente: $username@$host"
-            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
-CREATE USER IF NOT EXISTS '$username'@'$host' IDENTIFIED BY '$password';
-GRANT ALL PRIVILEGES ON \`$database\`.* TO '$username'@'$host';
-FLUSH PRIVILEGES;
-EOF
-            log "Utente '$username@$host' creato con password: $password"
-            ;;
-        "delete")
-            log "Eliminazione utente: $username@$host"
-            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
-DROP USER IF EXISTS '$username'@'$host';
-FLUSH PRIVILEGES;
-EOF
-            log "Utente '$username@$host' eliminato"
-            ;;
-        "reset_password")
-            log "Reset password per utente: $username@$host"
-            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
-ALTER USER '$username'@'$host' IDENTIFIED BY '$password';
-FLUSH PRIVILEGES;
-EOF
-            log "Password resettata per utente '$username@$host': $password"
-            ;;
-    esac
+percona_users() {
+  local action=${1:-""}
+  shift || true
+
+  case "$action" in
+    create)
+      require_params 2 "$@"   # deve avere almeno 2 parametri (username e password opzionale)
+      local user=$1
+      local pass=${2:-$(generate_password)}
+
+      log "Creating user $user"
+      mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "CREATE USER '$user'@'%' IDENTIFIED BY '$pass';"
+      mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "GRANT ALL ON *.* TO '$user'@'%';"
+      ;;
+    drop)
+      require_params 1 "$@"   # almeno 1 parametro: username
+      local user=$1
+
+      log "Dropping user $user"
+      mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "DROP USER IF EXISTS '$user'@'%';"
+      ;;
+    *)
+      error "Usage: $0 users {create|drop} <username> [password]"
+      ;;
+  esac
 }
