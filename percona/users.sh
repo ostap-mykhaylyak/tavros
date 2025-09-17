c1c964a08b54e0bbd3a73897217bba287a7f2444
Ostap Mykhaylyak <me@ostap.dev>
2025-09-17 03:54:10 +0200

    Update users.sh

--- percona/users.sh
+++ percona/users.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-ACTION=${1:-""}; USER=${2:-""}; PASS=${3:-$(generate_password)}
-case "$ACTION" in
-  create)
-    log "Creo utente $USER"
-    mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "CREATE USER '$USER'@'%' IDENTIFIED BY '$PASS';"
-    mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "GRANT ALL ON *.* TO '$USER'@'%';"
-    log "Utente creato con password: $PASS"
-    ;;
-  drop)
-    log "Elimino utente $USER"
-    mysql --defaults-file="$MYSQL_CONFIG_FILE" -e "DROP USER IF EXISTS '$USER'@'%';"
-    ;;
-  *)
-    error "Uso: $0 users {create|drop} <username> [password]"
-    ;;
-esac
+
+# Gestione utenti
+manage_user() {
+    local action="$1"
+    local username="$2"
+    local password="${3:-$(generate_password)}"
+    local database="${4:-*}"
+    local host="${5:-localhost}"
+    
+    case "$action" in
+        "create")
+            log "Creazione utente: $username@$host"
+            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
+CREATE USER IF NOT EXISTS '$username'@'$host' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON \`$database\`.* TO '$username'@'$host';
+FLUSH PRIVILEGES;
+EOF
+            log "Utente '$username@$host' creato con password: $password"
+            ;;
+        "delete")
+            log "Eliminazione utente: $username@$host"
+            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
+DROP USER IF EXISTS '$username'@'$host';
+FLUSH PRIVILEGES;
+EOF
+            log "Utente '$username@$host' eliminato"
+            ;;
+        "reset_password")
+            log "Reset password per utente: $username@$host"
+            mysql --defaults-file="$MYSQL_CONFIG_FILE" << EOF
+ALTER USER '$username'@'$host' IDENTIFIED BY '$password';
+FLUSH PRIVILEGES;
+EOF
+            log "Password resettata per utente '$username@$host': $password"
+            ;;
+    esac
+}
