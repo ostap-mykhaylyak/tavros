609862e73e56b48497091c0991349ba111895d64
Ostap Mykhaylyak <me@ostap.dev>
2025-09-17 03:32:44 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+MYSQL_ROOT_PASSWORD=""
+MYSQL_CONFIG_FILE="/root/.my.cnf"
+BACKUP_DIR="/var/backups/percona"
+
+CLUSTER_NAME="pxc_cluster"
+CLUSTER_CONFIG_FILE="/etc/mysql/mysql.conf.d/pxc.cnf"
+STANDARD_CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
+
+XTRABACKUP_USER="xtrabackup"
+XTRABACKUP_PASSWORD=""
+MONITOR_USER="monitor"
+MONITOR_PASSWORD=""
